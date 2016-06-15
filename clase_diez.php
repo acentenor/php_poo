ab82307c99de70622a316b89691c227e8660ee17
Alvaro Centeno <animetal81@hotmail.com>
2016-06-14 23:04:19 -0500

    Se crean archivos de la clase diez uso de spl_autoload_register()

---
+++ clase_diez.php
@@ -0,0 +1,9 @@
+<?php
+  function autoload($clase) {
+    include "clases/" . $clase . ".php";
+  }
+  spl_autoload_register('autoload');
+  Persona::mostrar("Hola Mundo");
+  Auto::mostrar("Hola Mundo");
+
+?>
